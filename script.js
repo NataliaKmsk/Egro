adbec60c0a0bbc54ae6be10d149fd4dc953dd7c0
NataliaKmsk <nataliak289@gmail.com>
2025-05-15 16:08:19 +0200

    4th commit

--- script.js
+++ script.js
@@ -5,3 +5,32 @@ if (contrastToggle) {
     document.body.classList.toggle("high-contrast");
   });
 }
+
+const toggleLight = document.getElementById("toggle-light");
+
+if (toggleLight) {
+  toggleLight.addEventListener("click", () => {
+    document.body.classList.toggle("light-mode");
+
+    if (document.body.classList.contains("light-mode")) {
+      localStorage.setItem("theme", "light");
+    } else {
+      localStorage.setItem("theme", "dark");
+    }
+  });
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  const savedTheme = localStorage.getItem("theme");
+  if (savedTheme === "light") {
+    document.body.classList.add("light-mode");
+  }
+});
+
+
+
+
+
+
+
+
